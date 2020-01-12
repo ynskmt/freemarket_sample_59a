163b38536af1601ec25f21f322c73f6a6020d76c
ynskmt <yrtiktwyr.0427@gmail.com>
2020-01-12 12:02:32 +0900

    create mypage top

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :products, only: :index
+
+  resources :mypages, only: [:index] do
+    collection do
+      get 'logout'
+    end
+  end
 end

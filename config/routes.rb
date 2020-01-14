ce67db7f5cb1b889f1b48f7b3aeb640b992ae860
ynskmt <yrtiktwyr.0427@gmail.com>
2020-01-14 15:39:50 +0900

    create session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+  }
+  
   root 'products#index'
   resources :products, only: [:index, :new]
 
@@ -9,4 +12,6 @@ Rails.application.routes.draw do
       get 'logout'
     end
   end
+
+  resources :users, only: [:index]
 end

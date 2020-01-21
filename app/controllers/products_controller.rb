bffda3d4413ff3b768a42b5c11b6e4a8656a67f1
ynskmt <yrtiktwyr.0427@gmail.com>
2020-01-21 12:22:03 +0900

    update category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,25 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @condition = Condition.all
-    @category = Category.all
+    # @category = Category.all
+    @category = []
+    Category.where(ancestry: nil).each do |parent|
+    @category << parent
+    end
     @delivery_charge = DeliveryCharge.all
     @delivery_way = DeliveryWay.all
     @prefecture = Prefecture.all
     @delivery_days = DeliveryDays.all
     @product.images.new
+  end
+
+  def category_children
+    children = Category.find(params[:name]).name
+    @category_children = Category.find_by(name: children, ancestry: nil ).children
+  end
+
+  def category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   def create
@@ -27,7 +40,7 @@ class ProductsController < ApplicationController
     params.require(:product).permit(
       :product_name,
       :product_description,
-      :category,
+      :category_id,
       :condition,
       :delivery_charge,
       :delivery_way,

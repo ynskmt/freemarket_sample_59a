5112caa680464d385c1e822e4db7db7aa4ed2f87
kt-ozi <9_t0rres_9@ezweb.ne.jp>
2020-01-23 21:15:34 +0900

    add association
    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day, presence: true
+  has_one :address
+  has_one :card
 end

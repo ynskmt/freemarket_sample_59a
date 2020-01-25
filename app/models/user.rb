74a865ab0b9da5c0b60a5080a36a1a8d16c87da2
kt-ozi <9_t0rres_9@ezweb.ne.jp>
2020-01-25 10:52:51 +0900

    add option and association
    userモデルに新たに追加したモデルのアソシエーションとオプションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+  
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day, presence: true
+  
   has_one :address
   has_one :card
 
+  has_many :sns_credentials
   has_many :products
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
+
 end

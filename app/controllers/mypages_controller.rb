df59c7ac0b287e7510a0df919cee02047224dbfb
misato0104 <tosamimayamika@gmail.com>
2020-02-02 17:00:24 +0900

    変更するのエラーを解消

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,2 +1,23 @@
 class MypagesController < ApplicationController
+
+  def index
+  end
+
+  def profile
+    @user = User.new
+  end
+
+  def update_profile
+    @user = User.find(params[:id])
+    current_user.update(user_params)
+    redirect_to profile_mypage_path(current_user.id)
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
+
+
 end

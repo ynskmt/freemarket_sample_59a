4513acad82beb209dc486b7d4d81de13f98db961
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-17 15:05:57 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,11 @@
 class PurchasesController < ApplicationController
   def show
     @product = Product.find(params[:id])
+    @card = current_user.card
+    if @card.present?
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_info = customer.cards.retrieve(@card.card_id)
+    end
   end
 end

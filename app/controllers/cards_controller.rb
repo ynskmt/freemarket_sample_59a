5a0556e69d2c1ef74108f99cfe6682236e9f2a57
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-19 00:57:05 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,22 +1,7 @@
 class CardsController < ApplicationController
   require 'payjp'
-  before_action :create_card, only: [:create, :pay]
-
+  
   def create
-    if @card.save
-      path = Rails.application.routes.recognize_path(request.referer)
-      if path[:controller] == "cards" && path[:action] == "new"
-        redirect_to controller: :users, action: :done
-      else
-        redirect_to done_users_path
-      end
-    else
-      redirect_to action: "pay"
-    end
-  end
-
-  private
-  def create_card
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
     if params['payjp-token'].blank?
       redirect_to action: "new"
@@ -27,29 +12,20 @@ class CardsController < ApplicationController
       card: params['payjp-token'],
       ) 
       @card = Card.new(user_id: user_id, customer_id: customer.id, card_id: customer.default_card)
+      path = Rails.application.routes.recognize_path(request.referer)
+      if @card.save
+        if path[:controller] == "cards" && path[:action] == "new"
+          redirect_to controller: :users, action: :done
+        elsif path[:controller] == "cards" && path[:action] == "new_card"
+          redirect_to controller: :mypages, action: :card
+        end
+      else
+        if path[:controller] == "cards" && path[:action] == "new"
+          redirect_to action: 'new'
+        elsif path[:controller] == "cards" && path[:action] == "new_card"
+          redirect_to controller: :mypages, action: :card
+        end
+      end
     end
   end
-  # def create_card
-  #   Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
-  #   if params['payjp-token'].blank?
-  #     redirect_to action: "new"
-  #   else
-  #     user_id = current_user.id
-  #     customer = Payjp::Customer.create(
-  #     description: '登録テスト', 
-  #     card: params['payjp-token'],
-  #     ) 
-  #     @card = Card.new(user_id: user_id, customer_id: customer.id, card_id: customer.default_card)
-  #     if @card.save
-  #       path = Rails.application.routes.recognize_path(request.referer)
-  #       if path[:controller] == "cards" && path[:action] == "new"
-  #         redirect_to controller: :users, action: :done
-  #       else
-  #         redirect_to done_users_path
-  #       end
-  #     else
-  #       redirect_to action: "pay"
-  #     end
-  #   end
-  # end
 end

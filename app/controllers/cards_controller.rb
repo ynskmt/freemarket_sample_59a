d58e6a5aad09b12e06902e8e4e46bc2b135ec34b
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-19 00:24:09 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,22 @@
 class CardsController < ApplicationController
   require 'payjp'
+  before_action :create_card, only: [:create, :pay]
 
-  def pay
+  def create
+    if @card.save
+      path = Rails.application.routes.recognize_path(request.referer)
+      if path[:controller] == "cards" && path[:action] == "new"
+        redirect_to controller: :users, action: :done
+      else
+        redirect_to done_users_path
+      end
+    else
+      redirect_to action: "pay"
+    end
+  end
+
+  private
+  def create_card
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
     if params['payjp-token'].blank?
       redirect_to action: "new"
@@ -12,16 +27,29 @@ class CardsController < ApplicationController
       card: params['payjp-token'],
       ) 
       @card = Card.new(user_id: user_id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        path = Rails.application.routes.recognize_path(request.referer)
-        if path[:controller] == "cards" && path[:action] == "new"
-          redirect_to controller: :users, action: :done
-        else
-          redirect_to done_users_path
-        end
-      else
-        redirect_to action: "pay"
-      end
     end
   end
+  # def create_card
+  #   Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+  #   if params['payjp-token'].blank?
+  #     redirect_to action: "new"
+  #   else
+  #     user_id = current_user.id
+  #     customer = Payjp::Customer.create(
+  #     description: '登録テスト', 
+  #     card: params['payjp-token'],
+  #     ) 
+  #     @card = Card.new(user_id: user_id, customer_id: customer.id, card_id: customer.default_card)
+  #     if @card.save
+  #       path = Rails.application.routes.recognize_path(request.referer)
+  #       if path[:controller] == "cards" && path[:action] == "new"
+  #         redirect_to controller: :users, action: :done
+  #       else
+  #         redirect_to done_users_path
+  #       end
+  #     else
+  #       redirect_to action: "pay"
+  #     end
+  #   end
+  # end
 end

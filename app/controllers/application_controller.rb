223290865c8a72cb67b34f027c69c945893517d7
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-19 12:21:41 +0900

    Revert "Update application_controller.rb"
    This reverts commit 2b391b271de00f8d045973dea53246dadddd1d8b.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_search
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day, :phone_num])
+  end
   
   private
-  def set_search
-    @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true)
-  end
 
   def production?
     Rails.env.production?
@@ -18,10 +20,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
-  end
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day, :phone_num])
   end
 end

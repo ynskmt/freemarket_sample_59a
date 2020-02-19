f6e5fc076ddf0baccdfdd7c6378e7ae4b6a9824c
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-19 14:48:48 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @ladies = Product.where(category_id:1..199).order("created_at DESC").limit(10)
+    # @ladies = Product.where(category_id:1..199).order("created_at DESC").limit(10)
+    @category = Category.find(params[:id])
+    if @category.has_children?
+      @category_products = Product.where(category_id: @category.descendant_ids).order("id DESC").limit(100)
+    else
+      @category_products = Product.where(category_id: @category.id).order("id DESC").limit(100)
+    end
   end
 end

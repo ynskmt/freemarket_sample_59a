9d1598b7b0e141b80d57fd1f2f02b68dd3637433
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-19 14:25:41 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,8 @@ class CategoriesController < ApplicationController
   def index
     @parent_categories = Category.where(ancestry: nil).order("id ASC")
   end
+
+  def show
+    @ladies = Product.where(category_id:1..199).order("created_at DESC").limit(10)
+  end
 end

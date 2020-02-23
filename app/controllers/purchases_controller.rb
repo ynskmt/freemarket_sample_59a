a5c94b90a64f8070f2e457ae7796b2aca8a347f9
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-23 19:08:32 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,13 +4,15 @@ class PurchasesController < ApplicationController
   require 'payjp'
 
   def show
+    @product = Product.find(params[:id])
+    redirect_to root_path if @product.user_id == current_user.id || @product.selling_status_id == "3"
     redirect_to card_mypages_path unless current_user.card
   end
 
   def pay
     card = current_user.card
     product = Product.find(params[:id])
-    if product.selling_status_id === "1"
+    if product.selling_status_id === "1" && product.user_id != current_user.id
       Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
         Payjp::Charge.create(
           amount: product.price,

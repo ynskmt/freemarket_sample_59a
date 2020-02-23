5929274f4b88642b86d603d5b7308fe1015a37f0
ynskmt <yrtiktwyr.0427@gmail.com>
2020-02-23 18:26:22 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,15 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  validates :user_id, presence: true
+  validates :product_name, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_way_id, presence: true
+  validates :delivery_area_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :price, presence: true
 end
